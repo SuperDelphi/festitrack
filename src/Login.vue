ca2145a2176df2aa29ab8d94ab1daccd31d1da21
SuperDelphi <44942598+SuperDelphi@users.noreply.github.com>
2026-02-08 21:32:49 +0100

    Add Supabase dependencies and create Login component

--- src/Login.vue
+++ src/Login.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Test" />
+    <h1>Login</h1>
 </template>
 
 <style scoped>
