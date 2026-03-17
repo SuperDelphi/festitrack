cdc19b082f24da4186e19f6ae1c600cfa330c675
SuperDelphi <44942598+SuperDelphi@users.noreply.github.com>
2026-03-17 13:20:06 +0100

    Router auth check

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/CounterDashboard.vue'
+import { useAuth } from '../composables/useAuth'
 
 const router = createRouter({
     history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -8,33 +9,48 @@ const router = createRouter({
             path: '/',
             name: 'home',
             component: HomeView,
-            meta: { showBottomBar: true }
+            meta: { showBottomBar: true, requiresAuth: true }
         },
         {
             path: '/login',
             name: 'login',
             component: () => import('../views/Login.vue'),
-            meta: { showBottomBar: false }
+            meta: { showBottomBar: false, requiresAuth: false }
         },
         {
             path: '/mediation',
             name: 'mediation',
             component: () => import('../views/Mediation.vue'),
-            meta: { showBottomBar: true }
+            meta: { showBottomBar: true, requiresAuth: true }
         },
         {
             path: '/account',
             name: 'account',
             component: () => import('../views/Account.vue'),
-            meta: { showBottomBar: true }
+            meta: { showBottomBar: true, requiresAuth: true }
         },
         {
             path: '/locations',
             name: 'locations',
             component: () => import('../views/Locations.vue'),
-            meta: { showBottomBar: false }
+            meta: { showBottomBar: false, requiresAuth: true }
         }
     ]
+})
+
+router.beforeEach(async (to, from, next) => {
+    const { sessionUser, init, isReady } = useAuth()
+
+    if (!isReady.value) {
+        await init()
+    }
+
+    // Si la page nécessite une auth et que la session est null -> Login
+    if (to.meta.requiresAuth && !sessionUser.value) {
+        next('/login')
+    } else {
+        next()
+    }
 })
 
 export default router
